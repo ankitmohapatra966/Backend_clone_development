2c419664052cdee677f3fd5a9409cdbd109c8455
ankitmohapatra966 <ankitmohapatra966@gmail.com>
2025-03-29 09:37:44 +0530

    Utils code Practice

--- src/index.js
+++ src/index.js
@@ -10,7 +10,18 @@ dotenv.config({
 })
 
 connectDB ()
-
+.then(() =>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+    app.on("error",(error) =>{
+        console.log("ERRR: ",error);
+        throw error
+    })
+})
+.catch((err) =>{
+    console.log("MongoDB Connection failed",err);
+})
 
 
 
